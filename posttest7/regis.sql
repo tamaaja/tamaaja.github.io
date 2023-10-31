31bce8a292673bac4e9e225e44815328abb7b3cc
MUHAMMAD RIZKY PUTRA PRATAMA <130198401+tamaaja@users.noreply.github.com>
2023-10-31 18:28:18 +0800

    Add files via upload

---
+++ posttest7/regis.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2023 at 10:47 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kelas_c1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regis`
+--
+
+CREATE TABLE `regis` (
+  `id` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `regis`
+--
+
+INSERT INTO `regis` (`id`, `username`, `password`) VALUES
+(1, 'ayamayam', '$2y$10$X5vDlibvHJ9Qb1DHJL.wmOUYMxl6ezMWazrpM9THXJC'),
+(2, 'bebek', '$2y$10$yk6.LnYjs0BVJaGa0eC1ru9fO/t2vFT96oj9.SFOPmv'),
+(3, '111', '$2y$10$AVo0.lm4473cZF2Y6Bcdy.bxsfZfawDMBcd8qR5IiBA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `regis`
+--
+ALTER TABLE `regis`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `regis`
+--
+ALTER TABLE `regis`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
